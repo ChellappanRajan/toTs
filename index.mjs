0b43d4440af47a0cb7fd7f554814a54552c4e826
chellappan@Chellappans-MBP.domain.name <chellapa@hutechsolutions.com>
2022-03-04 14:52:59 +0530

    first commit

---
+++ index.mjs
@@ -0,0 +1,46 @@
+#! /usr/bin/env node
+
+import chalk from 'chalk';
+import { compile } from "json-schema-to-typescript";
+import fs from "node:fs/promises";
+import path from "path";
+import toJsonSchema from "to-json-schema";
+
+const __dirname = path.resolve();
+const [, , command] = process.argv ;
+let filePath;
+let cmd;
+if(command?.split("=")){
+   [cmd, filePath] = command?.split("=");
+}
+
+switch(cmd){
+  case '--file':
+  case '-f':{
+    if(p){
+      try {
+        const json = await fs.readFile(filePath);
+        const jsoToTs = await compile(
+          { ...toJsonSchema(JSON.parse(json)), additionalProperties: false },
+          "test",
+          { bannerComment: "" }
+        );
+        const toFile = p.split(".").shift() || "test";
+        fs.writeFile(`${toFile}.ts`, jsoToTs);
+        console.log(`[${toFile}.ts] ${chalk.green('Interface file created succefully')} 🚀`);
+      } catch (error) {
+        console.log(chalk.red(`${error?.message}`));
+      }
+    }
+    break;
+  }
+  case '--version':
+  case '-v':{
+    const json = await fs.readFile('./package.json');
+    console.log(chalk.gray(`${JSON.parse(json)?.version}`));
+    break;
+  }
+  default:{
+    console.log(chalk.gray(`toTs --file=<filename.json>`));
+  }
+}
