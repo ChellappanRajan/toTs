8d546710b340de265fe02246228d4427f6c7d3d5
chellappan@Chellappans-MBP.domain.name <chellapa@hutechsolutions.com>
2022-03-05 11:24:56 +0530

    Lint configuration

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es2021': true,
+  },
+  'extends': [
+    'google',
+  ],
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module',
+  },
+  'rules': {
+    'indent': 'off',
+  },
+};
