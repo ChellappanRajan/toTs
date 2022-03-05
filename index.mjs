439a44a45aca99f4e4439f421b7fdb987f30d1e0
chellappan@Chellappans-MBP.domain.name <chellapa@hutechsolutions.com>
2022-03-05 11:24:32 +0530

    fix linting issue

--- index.mjs
+++ index.mjs
@@ -1,33 +1,34 @@
 #! /usr/bin/env node
 
 import chalk from 'chalk';
-import { compile } from "json-schema-to-typescript";
-import fs from "node:fs/promises";
-import path from "path";
-import toJsonSchema from "to-json-schema";
+import {compile} from 'json-schema-to-typescript';
+import fs from 'node:fs/promises';
+import toJsonSchema from 'to-json-schema';
 
-const __dirname = path.resolve();
-const [, , command] = process.argv ;
+const [, , command] = process.argv;
 let filePath;
 let cmd;
-if(command?.split("=")){
-   [cmd, filePath] = command?.split("=");
+if (command?.split('=')) {
+  [cmd, filePath] = command?.split('=');
 }
 
-switch(cmd){
+switch (cmd) {
   case '--file':
-  case '-f':{
-    if(filePath){
+  case '-f': {
+    if (filePath) {
       try {
         const json = await fs.readFile(filePath);
         const jsoToTs = await compile(
-          { ...toJsonSchema(JSON.parse(json)), additionalProperties: false },
-          "test",
-          { bannerComment: "" }
+          {...toJsonSchema(JSON.parse(json)), additionalProperties: false},
+          'test',
+          {bannerComment: ''},
         );
-        const toFile = filePath.split(".").shift() || "test";
+        const toFile = filePath.split('.').shift() || 'test';
         fs.writeFile(`${toFile}.ts`, jsoToTs);
-        console.log(`[${toFile}.ts] ${chalk.green('Interface file created succefully')} 🚀`);
+        console.log(
+          `[${toFile}.ts] ${chalk.green(
+            'Interface file created succefully')} 🚀`,
+        );
       } catch (error) {
         console.log(chalk.red(`${error?.message}`));
       }
@@ -35,12 +36,12 @@ switch(cmd){
     break;
   }
   case '--version':
-  case '-v':{
+  case '-v': {
     const json = await fs.readFile('./package.json');
     console.log(chalk.gray(`${JSON.parse(json)?.version}`));
     break;
   }
-  default:{
+  default: {
     console.log(chalk.gray(`toTs --file=<filename.json>`));
   }
 }
